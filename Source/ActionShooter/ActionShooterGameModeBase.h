239befc38c4c784390772b36138525ffdc46197a
CppUser <ganscorp@gmail.com>
2023-02-15 20:38:07 -0500

    Initial Commit

---
+++ Source/ActionShooter/ActionShooterGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ActionShooterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONSHOOTER_API AActionShooterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
